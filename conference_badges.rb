6a18fab41c13ea5a340fe12640365177eba8d5c1
Olivia Adongo <akinyioliviatove@gmail.com>
2022-08-31 08:45:39 +0300

    commit tree d60bd95ff461a5d3ba0b392ae49fea23be329042

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker (name)
+    return "Hello, my name is #{name}."
+    
+end
+
+def batch_badge_creator()
+    arr.map do |a|
+        return"Hello, my name  is #{a}."
+    end
+end
+
+def assign_rooms
+    
+end
+def printer
+    
+end
